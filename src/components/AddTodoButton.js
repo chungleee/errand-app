8ed76eda14b5ffe035596b90274656fc83ef2ca9
Leon <leon.karho.chung@gmail.com>
2019-09-16 15:56:41 -0400

    components folder

---
+++ src/components/AddTodoButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTodoButton = () => {
+  return (
+    <div className="h4 w4 mr3 ba br4 flex items-center justify-center tc grow">
+      <p>+</p>
+    </div>
+  )
+}
+
+export default AddTodoButton
---
+++ src/components/TodoCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TodoCard = () => {
+  return (
+    <div className="h4 w4 ba mr3 mb3 br4 flex items-center justify-center tc grow">
+      <p>this is a todo card</p>
+    </div>
+  )
+}
+export default TodoCard
