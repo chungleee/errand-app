a6e74c20b9ae4ae8a3a925c0193f1b7edcf91b42
Leon <leon.karho.chung@gmail.com>
2019-09-17 12:46:27 -0400

    test props passed from parents

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TodoCard = () => {
+const TodoCard = ({ title }) => {
   return (
     <div className="h4 w4 ba mr3 mb3 br4 flex items-center justify-center tc grow">
-      <p>this is a todo card</p>
+      <p>{title}</p>
     </div>
   )
 }
