13713a5148e6a2be8bda61faf9ddea124628504e
Leon <leon.karho.chung@gmail.com>
2019-09-17 15:02:02 -0400

    abstract Header and Section into their own components - convert functional to class component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Modal from 'react-modal'
 import AddTodoButton from './components/AddTodoButton'
 import TodoCard from './components/TodoCard'
+import Header from './components/Header'
+import TodoList from './components/TodoList'
 
-function App() {
-  const dummyData = [
-    {
-      title: 'leon',
-      tasks: ['task 1', 'task 2']
-    },
-    {
-      title: 'chung',
-      tasks: ['task 1', 'task 2']
-    }
-  ]
-  return (
-    <div>
-      <header className="tc mv4">
-        <h1 className="f2">Welcome to the Errands app</h1>
-        <p>Feel free to create your new list of errands!</p>
-      </header>
-      <section
-        className="flex flex-wrap ph4 justify-center mw6-ns"
-        style={{ margin: 'auto' }}
-      >
-        <AddTodoButton />
-
-        {dummyData.map((data, idx) => {
-          return <TodoCard key={idx} title={data.title} />
-        })}
-      </section>
-    </div>
-  )
+class App extends Component {
+  render() {
+    const dummyData = [
+      {
+        title: 'leon',
+        tasks: ['task 1', 'task 2']
+      },
+      {
+        title: 'chung',
+        tasks: ['task 1', 'task 2']
+      }
+    ]
+    return (
+      <div>
+        <Header />
+        <TodoList>
+          <AddTodoButton />
+        </TodoList>
+      </div>
+    )
+  }
 }
+
+// <section
+//   className="flex flex-wrap ph4 justify-center mw6-ns"
+//   style={{ margin: 'auto' }}
+// >
+//   <AddTodoButton />
+
+//   {dummyData.map((data, idx) => {
+//     return <TodoCard key={idx} title={data.title} />
+//   })}
+// </section>
 
 export default App
