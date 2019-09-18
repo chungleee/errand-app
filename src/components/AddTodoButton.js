9d5a7a2a7ed93744f7197ce943c97ec11733478a
Leon <leon.karho.chung@gmail.com>
2019-09-18 13:34:17 -0400

    modal for button

--- src/components/AddTodoButton.js
+++ src/components/AddTodoButton.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Modal from 'react-modal'
 
-const AddTodoButton = () => {
+const AddTodoButton = ({ handleOpenModal, handleCloseModal, showModal }) => {
   return (
-    <div className="h4 w4 mr3 ba br4 flex items-center justify-center tc grow">
-      <p>+</p>
+    <div>
+      <button
+        onClick={handleOpenModal}
+        className="h4 w4 mr3 ba br4 flex items-center justify-center tc grow bg-white b--black"
+      >
+        +
+      </button>
+      <Modal onRequestClose={handleCloseModal} isOpen={showModal}>
+        some content from AddTodoButton component
+      </Modal>
     </div>
   )
 }
