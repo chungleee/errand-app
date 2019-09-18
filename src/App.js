3cbbb47fee5cbe2ae1a17ca18a7e3f2cac762dcc
Leon <leon.karho.chung@gmail.com>
2019-09-18 13:33:59 -0400

    modal working for addtodobutton comp

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react'
 import Modal from 'react-modal'
-import AddTodoButton from './components/AddTodoButton'
-import TodoCard from './components/TodoCard'
 import Header from './components/Header'
 import TodoList from './components/TodoList'
+import AddTodoButton from './components/AddTodoButton'
+
+Modal.setAppElement('#root')
 
 class App extends Component {
+  state = {
+    showModal: false
+  }
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
+  }
+
   render() {
-    const dummyData = [
-      {
-        title: 'leon',
-        tasks: ['task 1', 'task 2']
-      },
-      {
-        title: 'chung',
-        tasks: ['task 1', 'task 2']
-      }
-    ]
     return (
       <div>
         <Header />
         <TodoList>
-          <AddTodoButton />
+          <AddTodoButton
+            showModal={this.state.showModal}
+            handleCloseModal={this.handleCloseModal}
+            handleOpenModal={this.handleOpenModal}
+          />
         </TodoList>
       </div>
     )
   }
 }
-
-// <section
-//   className="flex flex-wrap ph4 justify-center mw6-ns"
-//   style={{ margin: 'auto' }}
-// >
-//   <AddTodoButton />
-
-//   {dummyData.map((data, idx) => {
-//     return <TodoCard key={idx} title={data.title} />
-//   })}
-// </section>
 
 export default App
