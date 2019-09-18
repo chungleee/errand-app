05c96257ecf398439beda3d5fd7531978cf4db8b
Leon <leon.karho.chung@gmail.com>
2019-09-18 16:07:34 -0400

    introducing hooks? hmm

--- src/components/AddTodoButton.js
+++ src/components/AddTodoButton.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
 
 const AddTodoButton = ({ handleOpenModal, handleCloseModal, showModal }) => {
+  const [title, setTitle] = useState('')
+  const [task, setTask] = useState('')
+
   return (
     <div>
       <button
@@ -15,17 +18,29 @@ const AddTodoButton = ({ handleOpenModal, handleCloseModal, showModal }) => {
         <form className="mt3 mw6-ns flex flex-column items-center">
           <input
             type="text"
+            name="title"
+            onClick={e => {
+              setTitle(e.target.value)
+            }}
             className="input-reset mb2 w-75 h2 f3 tc tracked fw6 i"
             style={{ border: 'none', borderBottom: '1px solid black' }}
             placeholder="Title"
           />
           <input
             type="text"
+            name="task"
+            onClick={e => {
+              setTask(e.target.value)
+            }}
             className="input-reset w-75 h2 f6 tc tracked fw6 i"
             style={{ border: 'none', borderBottom: '1px solid black' }}
             placeholder="Task"
           />
         </form>
+        <div>
+          <h1 className="f2">{title}</h1>
+          <ul>{task.length > 0 ? <li>{task}</li> : null}</ul>
+        </div>
       </Modal>
     </div>
   )
