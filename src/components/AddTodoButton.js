7efaa4261499d042364ecdbaff2f7238e423dda8
Leon <leon.karho.chung@gmail.com>
2019-09-24 13:25:45 -0400

    refactor modal trigger handle prop func

--- src/components/AddTodoButton.js
+++ src/components/AddTodoButton.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 
-const AddTodoButton = ({ handleOpenModal, handleCloseModal, showModal }) => {
-  const [title, setTitle] = useState('')
-  const [task, setTask] = useState('')
-
+const AddTodoButton = ({ handleModal, showModal }) => {
   return (
     <div>
       <button
-        onClick={handleOpenModal}
+        onClick={handleModal}
         className="h4 w4 mr3 ba br4 flex items-center justify-center tc grow bg-white b--black"
       >
         +
       </button>
-      <Modal onRequestClose={handleCloseModal} isOpen={showModal}>
+      <Modal onRequestClose={handleModal} isOpen={showModal}>
         <h1 className="f2 tc">Organise your errands</h1>
         <form className="mt3 mw6-ns flex flex-column items-center">
           <input
             type="text"
             name="title"
-            onClick={e => {
-              setTitle(e.target.value)
-            }}
             className="input-reset mb2 w-75 h2 f3 tc tracked fw6 i"
             style={{ border: 'none', borderBottom: '1px solid black' }}
             placeholder="Title"
@@ -29,17 +23,14 @@ const AddTodoButton = ({ handleOpenModal, handleCloseModal, showModal }) => {
           <input
             type="text"
             name="task"
-            onClick={e => {
-              setTask(e.target.value)
-            }}
             className="input-reset w-75 h2 f6 tc tracked fw6 i"
             style={{ border: 'none', borderBottom: '1px solid black' }}
             placeholder="Task"
           />
+          <input type="submit" value="add task" />
         </form>
         <div>
-          <h1 className="f2">{title}</h1>
-          <ul>{task.length > 0 ? <li>{task}</li> : null}</ul>
+          <h1 className="f2">{}</h1>
         </div>
       </Modal>
     </div>
