777fe548f71cc962d48c76033c37bfd82fe8b004
Leon <leon.karho.chung@gmail.com>
2019-09-25 16:25:17 -0400

    can get data from localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal'
 import Header from './components/Header'
 import TodoList from './components/TodoList'
@@ -13,6 +13,28 @@ const App = () => {
   const handleModal = () => {
     setModal(!modal)
   }
+
+  // ERRANDS
+  // errands state
+  const [errands, setErrands] = useState([])
+  useEffect(() => {
+    // check if localstorage exists
+    if (!window.localStorage) {
+      return null
+    } else {
+      // loop localstorage
+      for (const key in localStorage) {
+        if (localStorage.hasOwnProperty(key)) {
+          // parse each key
+          const json = JSON.parse(localStorage[key])
+
+          // update state
+          setErrands(errands.push(json))
+        }
+      }
+    }
+  }, [])
+
   return (
     <div>
       <Header />
