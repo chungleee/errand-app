b36ab51b34617e4428d223214763c2cea8fc087d
Leon <leon.karho.chung@gmail.com>
2019-09-30 10:15:39 -0400

    remove effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
 import Header from './components/Header'
 import TodoList from './components/TodoList'
@@ -13,27 +13,6 @@ const App = () => {
   const handleModal = () => {
     setModal(!modal)
   }
-
-  // ERRANDS
-  const [errands, setErrands] = useState([])
-  // function to get data from localStorage
-  const fetchStorage = () => {
-    if (!window.localStorage) {
-      alert(new Error("Sorry, local storage isn't available right now"))
-    } else {
-      for (const key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-          const json = JSON.parse(localStorage[key])
-          setErrands(errands.push(json))
-        }
-      }
-    }
-    console.log(errands)
-  }
-
-  useEffect(() => {
-    fetchStorage()
-  }, [])
 
   return (
     <div>
