96d72e6244aecd2cec8ec524a51e8a24f242e7ce
Leon <leon.karho.chung@gmail.com>
2019-10-02 13:45:39 -0400

    local modal state and handler - pass props into modal

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 
-const TodoCard = ({ title }) => {
+const TodoCard = ({ title, todos }) => {
+  // MODAL
+  // modal state
+  const [modal, setModal] = useState(false)
+  // modal trigger func
+  const handleModal = () => {
+    setModal(!modal)
+  }
   return (
-    <div className="h4 w4 ba mr3 mb3 br4 flex items-center justify-center tc grow">
-      <p className="b">{title}</p>
+    <div
+      onClick={handleModal}
+      className="h4 w4 ba mr3 mb3 br4 flex items-center justify-center tc grow"
+    >
+      <div>
+        <p className="b">{title}</p>
+      </div>
+      <Modal
+        onRequestClose={() => {
+          handleModal()
+        }}
+        isOpen={modal}
+      >
+        <h1>{title}</h1>
+        {todos.map((todo, idx) => {
+          return <li key={idx}>{todo}</li>
+        })}
+      </Modal>
     </div>
   )
 }
