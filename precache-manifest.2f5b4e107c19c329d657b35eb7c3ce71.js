9e56470a9812a8b7517f6251793bbdb3fb602ec8
Leon <leon.karho.chung@gmail.com>
2019-10-07 14:43:42 -0400

    Updates

---
+++ precache-manifest.2f5b4e107c19c329d657b35eb7c3ce71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06bfce748850ea54ecf3625a17586b6",
+    "url": "/errand-app/index.html"
+  },
+  {
+    "revision": "6c7b4edf2ad8e5c0e7f0",
+    "url": "/errand-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9e33a7ff31e7e66ed878",
+    "url": "/errand-app/static/css/main.4e73ef05.chunk.css"
+  },
+  {
+    "revision": "6c7b4edf2ad8e5c0e7f0",
+    "url": "/errand-app/static/js/2.94638094.chunk.js"
+  },
+  {
+    "revision": "9e33a7ff31e7e66ed878",
+    "url": "/errand-app/static/js/main.d93d518d.chunk.js"
+  },
+  {
+    "revision": "951ae63712edb1dbf7cf",
+    "url": "/errand-app/static/js/runtime~main.c0e1a857.js"
+  }
+]);
